7382fe41c23ef3897d94f50caf7533657aa69ece
chuzhumin98 <chuzhumin98@gmail.com>
2018-01-07 22:35:34 +0800

    start to read train file

--- src/myHMM/ReadTrain.java
+++ src/myHMM/ReadTrain.java
@@ -1,9 +1,15 @@
 package myHMM;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 
 public class ReadTrain {
-	private final static String trainPath = "icwb2-data/pku_training.utf8"; //存储的训练集的地址
+	private final static String trainPath = "icwb2-data/training/pku_training.utf8"; //存储的训练集的地址
 	
 	public double[][] A; //状态转移概率矩阵
 	public int N; //状态个数
@@ -20,7 +26,7 @@ public class ReadTrain {
 	 * 
 	 * @return
 	 */
-	public ReadTrain getInstance() {
+	public static ReadTrain getInstance() {
 		if (myTrain == null) {
 			myTrain = new ReadTrain();
 		}
@@ -45,6 +51,26 @@ public class ReadTrain {
 	 * 读取训练集
 	 */
 	private void readFile() {
-		
+		InputStreamReader isr;
+		BufferedReader br;
+		try {
+			isr = new InputStreamReader(new FileInputStream(ReadTrain.trainPath), "utf-8");
+			br = new BufferedReader(isr);
+			String line;
+			while(true) {
+				line = br.readLine();
+				if (line == null) {
+					break;
+				}
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		ReadTrain train = ReadTrain.getInstance();
 	}
 }
